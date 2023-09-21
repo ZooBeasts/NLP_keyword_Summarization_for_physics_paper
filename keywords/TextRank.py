0e5d5808930b2098eefcb5e534d685cd0561cbbd
Ziheng Guo <75404784+ZooBeasts@users.noreply.github.com>
2023-09-21 22:37:10 +0100

    Add files via upload

--- keywords/TextRank.py
+++ keywords/TextRank.py
@@ -1,26 +1,17 @@
-
-
-
-
-class UndirectedWeightedgraph():
+class UndirectedWeightedgraph:
 
     def __init__(self, iter_num=15):
         self.iter_num = iter_num
         self.graph = {}
         self.d = 0.85
 
-
-
-
-    def add_edge(self, start, end, weight):
-        if start not in self.graph:
-            self.graph[start] = []
-        if end not in self.graph:
-            self.graph[end] = []
-        self.graph[start].append((start, end, weight))
-        self.graph[end].append((end, start, weight))
-
-
+    def add_edge(self, start_node, end_node, weight):
+        if start_node not in self.graph:
+            self.graph[start_node] = []
+        if end_node not in self.graph:
+            self.graph[end_node] = []
+        self.graph[start_node].append((start_node, end_node, weight))
+        self.graph[end_node].append((end_node, start_node, weight))
 
     def rank(self):
         node_ranks = {}
@@ -29,37 +20,45 @@ class UndirectedWeightedgraph():
         init_rank = 1.0 / (len(self.graph) or 1.0)
         for node, edges in self.graph.items():
             node_ranks[node] = init_rank
-            node_edge_sum_weight[node] = sum([edge[2] for edge in edges])
-        nodes = sorted(self.graph.keys())
+            node_edge_sum_weight[node] = sum((edge[2] for edge in edges))
 
+        nodes = sorted(self.graph.keys())
         for iter_i in range(self.iter_num):
             for node in nodes:
                 _sum = 0
-                for (_,neighbor_node, weight) in self.graph[node]:
+                for (_, neighbor_node, weight) in self.graph[node]:
                     _sum += weight / node_edge_sum_weight[neighbor_node] * node_ranks[neighbor_node]
                 node_ranks[node] = (1 - self.d) + self.d * _sum
 
-        min_rank, max_rank = min(node_ranks.values()), max(node_ranks.values())
-        for node, rank_val in node_ranks.items():
-            node_ranks[node] = (rank_val - min_rank / (max_rank - min_rank))
+        if node_ranks:
+            max_rank = max(node_ranks.values())
+            min_rank = min(node_ranks.values())
+            for node, rank_val in node_ranks.items():
+                node_ranks[node] = (rank_val - min_rank) / (max_rank - min_rank)
+
+
+        # max_rank = max(node_ranks.values())
+        # min_rank = min(node_ranks.values())
+        # for node, rank_val in node_ranks.items():
+        #     node_ranks[node] = (rank_val - min_rank / (max_rank - min_rank))
+
         return node_ranks
 
 
-class Textrank():
+class Textrank:
     @staticmethod
-    def _calculate_graph(element_relation):
+    def _calculate_graph(elements_relation):
         graph = UndirectedWeightedgraph()
 
-        for (word1,word2), freq in element_relation.item():
+        for (word1, word2), freq in elements_relation.items():
             graph.add_edge(word1, word2, freq)
 
         nodes_rank = graph.rank()
 
-        results = sorted(nodes_rank.items(), key=lambda x: x[1], reverse=True)
-
+        results = sorted(nodes_rank.items(), key=lambda item: item[1], reverse=True)
         return results
 
-    def textrank(self, elements, window_size = 10):
+    def textrank(self, elements, window_size=3):
 
         elements_relation = {}
 
@@ -72,4 +71,5 @@ class Textrank():
 
         return result
 
-
+c = UndirectedWeightedgraph()
+c.rank()
