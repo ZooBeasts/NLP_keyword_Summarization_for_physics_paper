2b3cec56e3beb7bc272c01f0d787e396f58def1a
Ziheng Guo <75404784+ZooBeasts@users.noreply.github.com>
2023-09-22 14:03:32 +0100

    Add files via upload

--- test/test_Keywords.py
+++ test/test_Keywords.py
@@ -1,33 +1,44 @@
 import unittest
 from keywords.Keywords import Keyword
-
-
+import keywords.word_splitter as Textsplitter
+import numpy as np
 class TestKeyword(unittest.TestCase):
 
 
     def setUp(self):
-        self.keywords = Keyword(" path ")
+        self.keywords = Keyword('C:/pythonProject/test/idf.txt')
 
-    def test_textrank(self):
-        print('test_textrank------------------')
-        title = ''
-        content = 'Today is new day, I am very happy, good morning'
-        for top_k in range(5):
-            keywords = self.keyword.extract(title, content, top_k=2, method='textrank',with_weight=False)
-            print(keywords)
-            keywords = self.keyword.extract(title, content, method='textrank',with_weight=True)
-            print(keywords)
+    # def test_textrank(self):
+    #     print('test_textrank------------------')
+    #     title = ''
+    #     content = 'Today is new day, I am very happy, good morning'
+    #     for top_k in range(5):
+    #         keywords = self.keyword.extract(title, content, top_k=2, method='textrank',with_weight=False)
+    #         print(keywords)
+    #         keywords = self.keyword.extract(title, content, method='textrank',with_weight=True)
+    #         print(keywords)
 
 
     def test_tfidf(self):
         print('test_tfidf------------------')
         title = ''
-        content = 'Today is new day, I am very happy, good morning'
-        for top_k in range(5):
-            keywords = self.keyword.extract(title, content, top_k=2, method='tfidf',with_weight=False)
-            print(keywords)
-            keywords = self.keyword.extract(title, content, method='tfidf',with_weight=True)
-            print(keywords)
+        with open ('C:/pythonProject/test/data/data2.txt','r',encoding='UTF-8') as f:
+            content = f.readlines()
+            content = ''.join(content)
+            content = Textsplitter.Textsplitter().split_sentences_for_seg(content)
+            content = ''.join(content)
+
+            for top_k in range(6):
+                keywords = self.keywords.extract(title, content, top_k=6, method='TFIDF', with_weight=False)
+                print(keywords)
+                print('------------------\n')
+                keywords = self.keywords.extract(title, content, top_k=6, method='TFIDF', with_weight=True)
+                print(keywords)
+
+            for i in keywords:
+                print(i[0])
+
+
 
 
 if __name__ == '__main__':
--- test/test_tfidf.py
+++ test/test_tfidf.py
@@ -7,8 +7,8 @@ import os
 class TestKeyword(unittest.TestCase):
     def setUp(self):
         self.tfidf = TFIDF()
-        self.seg_data_dir = 'C:/Users/pythonProject/test/data'
-        self.idf_file_path = 'C:/Users/pythonProject/test/idf.txt'
+        self.seg_data_dir = r'C:/pythonProject/test/data/'
+        self.idf_file_path = r'C:/pythonProject/test/idf.txt'
 
     def test_tf_idf(self):
         print('test_train_idf------------------')
@@ -16,7 +16,7 @@ class TestKeyword(unittest.TestCase):
         self.tfidf.train_idf(seg_files= seg_files, output_file_name= self.idf_file_path)
 
         print('test_load_idf------------------')
-        words = ['me', 'you', 'he', 'she', 'it', 'they', 'us', 'them']
+        words = ['waveguide']
         self.tfidf.load_idf(self.idf_file_path)
         result = self.tfidf.compute_tfidf(words)
         print(result)
